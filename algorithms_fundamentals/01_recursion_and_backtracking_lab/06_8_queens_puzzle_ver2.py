71e24a907ff7e7df3749e709fdb4703c853d42c5
Yavor Gornalov <116191485+yavor-gornalov@users.noreply.github.com>
2023-06-15 07:35:06 +0300

    Update 06_8_queens_puzzle_ver2.py

--- algorithms_fundamentals/01_recursion_and_backtracking_lab/06_8_queens_puzzle_ver2.py
+++ algorithms_fundamentals/01_recursion_and_backtracking_lab/06_8_queens_puzzle_ver2.py
@@ -1,54 +1,55 @@
 # https://judge.softuni.org/Contests/Practice/Index/3459#5
-
-QUEEN, FREE = "*", "-"
-BOARD_SIZE = 8
-
-
+"""
+TODO: Not finished, yet
+"""
 def print_board(board):
-    for i in range(BOARD_SIZE):
-        print(*board[i], sep=" ")
+    for row in board:
+        print(" ".join(row))
     print()
 
 
-def put_queen(row, col, rows, cols, left_diagonals, right_diagonals):
-    chessboard[row][col] = QUEEN
-    rows.add(row)
-    cols.add(col)
-    left_diagonals.add(row - col)
-    right_diagonals.add(row + col)
+def set_attacked_fields(board, queen_row, queen_coll, symbol):
+    board[queen_row] = [symbol] * SIZE
+    for r in range(queen_row, SIZE):
+        board[r][queen_coll] = symbol
+    r, c = queen_row, queen_coll
+    while r < SIZE and c < SIZE:
+        board[r][c] = symbol
+        r += 1
+        c += 1
+    r, c = queen_row, queen_coll
+    while r < SIZE and c >= 0:
+        board[r][c] = symbol
+        r += 1
+        c -= 1
 
 
-def remove_queen(row, col, rows, cols, left_diagonals, right_diagonals):
-    chessboard[row][col] = FREE
-    rows.remove(row)
-    cols.remove(col)
-    left_diagonals.remove(row - col)
-    right_diagonals.remove(row + col)
+def is_set_queen_possible(board, row, col): return board[row][col] == FREE
 
 
-def is_queen_position_attacked(row, col, rows, cols, left_diagonals, right_diagonals):
-    if row in rows:
-        return True
-    if col in cols:
-        return True
-    if row - col in left_diagonals:
-        return True
-    if row + col in right_diagonals:
-        return True
-    return False
+def set_queen(board, row, col):
+    set_attacked_fields(board, row, col, ATTACKED)
+    board[row][col] = QUEEN
 
 
-def put_queens(board, row, rows, cols, left_diagonals, right_diagonals):
-    if row == BOARD_SIZE:
+def reset_queen(board, row, col):
+    set_attacked_fields(board, row, col, FREE)
+
+
+def put_queens(board, row):
+    if row >= SIZE:
         print_board(board)
         return
+    for col in range(SIZE):
+        if is_set_queen_possible(board, row, col):
+            set_queen(board, row, col)
+            put_queens(board, row + 1)
+            reset_queen(board, row, col)
 
-    for col in range(BOARD_SIZE):
-        if not is_queen_position_attacked(row, col, rows, cols, left_diagonals, right_diagonals):
-            put_queen(row, col, rows, cols, left_diagonals, right_diagonals)
-            put_queens(board, row + 1,  rows, cols, left_diagonals, right_diagonals)
-            remove_queen(row, col, rows, cols, left_diagonals, right_diagonals)
 
+SIZE = 8
+QUEEN, FREE, ATTACKED = "*", "-", "+"
+chessboard = []
+[chessboard.append([FREE] * SIZE) for _ in range(SIZE)]
 
-chessboard = [[FREE] * 8 for _ in range(BOARD_SIZE)]
-put_queens(chessboard, 0, set(), set(), set(), set())
+put_queens(chessboard, 0)
