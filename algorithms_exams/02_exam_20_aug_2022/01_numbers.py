623436482602add085f8d799f12a384f70eaaeb2
Yavor Gornalov <y.gornalov@gmail.com>
2023-06-25 08:30:56 +0300

    01_numbers.py - modified

--- algorithms_exams/02_exam_20_aug_2022/01_numbers.py
+++ algorithms_exams/02_exam_20_aug_2022/01_numbers.py
@@ -1,34 +1,19 @@
-# https://judge.softuni.org/Contests/Practice/Index/3623#0
+# # https://judge.softuni.org/Contests/Practice/Index/3623#0
 
-def find_sum_combinations(num, current_sum, numbers, combination, all_combinations):
-    if num == 0:
-        all_combinations.append([0])
-        return
-    elif current_sum == num:
-        if sorted(combination, reverse=True) not in all_combinations:
-            all_combinations.append(combination)
-        return
-    elif current_sum > num:
+def print_combinations(n, current_combination):
+    if n == 0:
+        print(" + ".join(str(num) for num in current_combination))
         return
 
-    for n in numbers:
-        new_sum = current_sum + n
-        if new_sum <= num:
-            new_combination = combination + [n]
-            find_sum_combinations(num, new_sum, numbers, new_combination, all_combinations)
+    if current_combination:
+        combination_range = min(n, current_combination[-1])
+    else:
+        combination_range = n
+
+    for i in range(combination_range, 0, -1):
+        print_combinations(n - i, current_combination + [i])
 
 
-def find_all_sum_combinations(num):
-    numbers = [i for i in range(num, 0, - 1)]
-    combination = []  # Initialize an empty combination
-    all_combinations = []
-    find_sum_combinations(num, 0, numbers, combination, all_combinations)
-    return all_combinations
-
-
-# Example usage:
 number = int(input())
-results = find_all_sum_combinations(number)
-
-for result in results:
-    print(*result, sep=" + ")
+combination = []
+print_combinations(number, [])
