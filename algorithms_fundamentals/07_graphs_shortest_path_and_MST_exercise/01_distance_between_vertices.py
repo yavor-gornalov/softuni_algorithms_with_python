9c25625743f727af976f977094a864729997b34d
Yavor Gornalov <y.gornalov@gmail.com>
2023-06-27 12:55:54 +0300

    01_distance_between_vertices.py - finished

--- algorithms_fundamentals/07_graphs_shortest_path_and_MST_exercise/01_distance_between_vertices.py
+++ algorithms_fundamentals/07_graphs_shortest_path_and_MST_exercise/01_distance_between_vertices.py
@@ -4,28 +4,29 @@ from collections import deque
 
 
 def shortest_path(start_node, end_node, graph):
-    visited = [False] * len(graph)
-    parent = [None] * len(graph)
-
     queue = deque([start_node])
-    visited[start_node] = True
+    visited = {start_node}
+    parent = {end_node: None}
 
     while queue:
         node = queue.popleft()
         if node == end_node:
             break
         for child in graph[node]:
-            if not visited[child]:
-                visited[child] = True
+            if child not in visited:
+                visited.add(child)
                 queue.append(child)
                 parent[child] = node
 
-    result = -1
-    if parent[end_node] is not None:
-        node = end_node
-        while node is not None:
-            node = parent[node]
-            result += 1
+    if parent[end_node] is None:
+        return -1
+    else:
+        result = 0
+        if parent[end_node] is not None:
+            node = end_node
+            while node in parent:
+                node = parent[node]
+                result += 1
 
     return result
 
@@ -33,14 +34,12 @@ def shortest_path(start_node, end_node, graph):
 nodes_count = int(input())
 number_of_pairs = int(input())
 
-graph = []
-[graph.append([]) for _ in range(nodes_count + 1)]
-for _ in range(1, nodes_count + 1):
+graph = {}
+for _ in range(nodes_count):
     node, edges_str = input().split(":")
-    node = int(node)
-    graph[node] = [int(e) for e in edges_str.split()] if edges_str else []
+    graph[node] = edges_str.split() if edges_str else []
 
 for _ in range(number_of_pairs):
-    first, second = [int(x) for x in input().split("-")]
+    first, second = input().split("-")
     path_length = shortest_path(first, second, graph)
     print(f"{{{first}, {second}}} -> {path_length}")
