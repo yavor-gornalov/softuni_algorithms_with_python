8ee0b5af9ec96e25b4d5bec3fa9813484b6aed5f
Yavor Gornalov <y.gornalov@gmail.com>
2023-07-07 20:27:53 +0300

    04_monkey_job.py - updated

--- algorithms_fundamentals/10_exam_preparation/04_monkey_job.py
+++ algorithms_fundamentals/10_exam_preparation/04_monkey_job.py
@@ -1,21 +1,26 @@
+# https://judge.softuni.org/Contests/Practice/Index/2481#0
+
 """
 This is a problem from Algorithms with Java course.
 Python to Java converter used for testing the code in judge system
 """
 
-def monkey_job(vect, idx, solution):
+def monkey_job(vect, idx, solution, output):
     if idx == len(vect):
         if sum(solution) == 0:
-            print(*["+" + str(solution[i]) if solution[i] > 0 else str(solution[i]) for i in range(len(solution))])
+            # print(*["+" + str(solution[i]) if solution[i] > 0 else str(solution[i]) for i in range(len(solution))])
+            # return 1
+            string = ["+" + str(solution[i]) if solution[i] > 0 else str(solution[i]) for i in range(len(solution))]
+            output.append(string)
             return 1
         return 0
 
     counter = 0
     solution[idx] = vect[idx]
-    counter += monkey_job(vect, idx + 1, solution)
+    counter += monkey_job(vect, idx + 1, solution, output)
 
     solution[idx] = -vect[idx]
-    counter += monkey_job(vect, idx + 1, solution)
+    counter += monkey_job(vect, idx + 1, solution, output)
 
     return counter
 
@@ -38,5 +43,8 @@ def monkey_job(vect, idx, solution):
 n = int(input())
 numbers = [x for x in range(1, n + 1)]
 solution = [0] * n
-total_solutions = monkey_job(vect=numbers, idx=0, solution=solution)
+output = []
+total_solutions = monkey_job(vect=numbers, idx=0, solution=solution, output=output)
+for result in output:
+    print(' '.join(result))
 print(f"Total Solutions: {total_solutions}")
